ced7e3ad7c93593eeaccd01b49095cec114945be
Jose Jimenez Jr <josejimenezjr0@gmail.com>
2017-06-25 22:43:43 -0500

    Add third answer result

--- logsanalysis.py
+++ logsanalysis.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import psycopg2
+from datetime import date
+import dateutil.parser
 
 db = psycopg2.connect("dbname=news user=postgres host=127.0.0.1 password=postgres port=5432")
 c = db.cursor()
@@ -40,3 +42,18 @@ rows = c.fetchall()
 
 for i in rows:
     print("'{s[0]}' - {s[1]} views".format(s=i))
+
+query = """
+        select date_trunc('day', time)::date as day,
+            round(sum(case when status != '200 OK' then 1 end)::numeric / count(status) * 100, 2) as "% errors"
+        from log
+        group by day
+        order by "% errors" desc
+        limit 1;
+        """
+c.execute(query)
+rows = c.fetchall()
+
+date = rows[0][0].strftime("%B %d, %Y")
+error_percent = rows[0][1]
+print("{} - {}% errors".format(date, error_percent))
